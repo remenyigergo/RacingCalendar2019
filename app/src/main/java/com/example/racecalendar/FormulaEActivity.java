d47584278c1e567f1656edfdb361c2dba79e6e11
greg.remenyi <greg.remenyi@gmail.com>
2019-07-14 14:30:10 +0200

    Initial commit. Two activity added (Formula-E, Formula-1) to Home and Gallery menus. Working state.
    Next issue: make main activity not blank.

---
+++ app/src/main/java/com/example/racecalendar/FormulaEActivity.java
@@ -0,0 +1,18 @@
+package com.example.racecalendar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FormulaEActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.formula_e, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/racecalendar/FormulaOneActivity.java
@@ -0,0 +1,19 @@
+package com.example.racecalendar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FormulaOneActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.formula_one, container, false);
+    }
+
+}
